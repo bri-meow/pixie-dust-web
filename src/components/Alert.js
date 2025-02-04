8638fcb91a3b8ea7ebc321a32902a963199a179b
bri-meow <bri@local.local>
2025-02-03 17:11:24 -0800

    🏰 lots of things

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -11,11 +11,15 @@ import {
 } from "firebase/firestore";
 
 import moment from "moment";
-import dayjs from "dayjs";
 
 import {
   Box,
   Button,
+  Chip,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   Stack,
   FormControl,
   InputLabel,
@@ -32,8 +36,13 @@ import {
   MenuItem,
   Typography,
 } from "@mui/material";
-import { Delete, EmojiEvents, NotificationsActive } from "@mui/icons-material";
-import { grey } from "@mui/material/colors";
+import {
+  Delete,
+  PlayCircle,
+  MenuBookTwoTone,
+  NotificationsActive,
+} from "@mui/icons-material";
+import { blue, grey } from "@mui/material/colors";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
@@ -60,13 +69,26 @@ const Alert = () => {
   const { user, logout } = useContext(AuthContext);
   const theme = useTheme();
   const isSmall = useMediaQuery(theme.breakpoints.down("sm"));
-  const defaultDate = dayjs().add(1, "day");
+  const defaultDate = null;
   const [park, setPark] = useState("Both");
   const [notificationUser, setNotificationUser] = useState(
     user.displayName.split(" ")[0].toLowerCase()
   );
   const [alerts, setAlerts] = useState([]);
   const [date, setDate] = React.useState(defaultDate);
+  const [alertToDelete, setAlertToDelete] = useState(null);
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+  const [alertForDetails, setAlertForDetails] = useState(null);
+  const [openDetailsModal, setOpenDetailsModal] = useState(false);
+  const handleCloseDeleteModal = () => {
+    setAlertToDelete(null);
+    setOpenDeleteModal(false);
+  };
+
+  const handleCloseDetailsModal = () => {
+    setAlertForDetails(null);
+    setOpenDetailsModal(false);
+  };
 
   const isButtonDisabled = !park || !date || !notificationUser;
 
@@ -96,6 +118,7 @@ const Alert = () => {
     try {
       await deleteDoc(doc(db, "test-alerts", documentId));
       console.log("Document successfully deleted!");
+      handleCloseDeleteModal();
     } catch (e) {
       console.error("Error deleting document: ", e);
     }
@@ -131,12 +154,22 @@ const Alert = () => {
           ...doc.data(),
           id: doc.id,
         }));
-        setAlerts(newData);
+        setAlerts(newData.sort((a, b) => new Date(a.date) - new Date(b.date)));
       }
     );
 
     return () => unsubscribe(); // Unsubscribe when component unmounts
   }, []);
+
+  useEffect(() => {
+    if (alertToDelete) {
+      setOpenDeleteModal(true);
+    }
+
+    if (alertForDetails) {
+      setOpenDetailsModal(true);
+    }
+  }, [alertToDelete, alertForDetails]);
 
   return (
     <Box py={2} px={{ xs: 0.5, sm: 2 }}>
@@ -145,23 +178,51 @@ const Alert = () => {
         spacing={2}
         justifyContent="center"
       >
-        <FormControl sx={{ m: 1, minWidth: 250 }}>
+        <FormControl sx={{ m: 1, minWidth: 300 }}>
           <InputLabel id="park-label">Park</InputLabel>
           <Select
             labelId="park-label"
             id="park-select"
             value={park}
             label="Park"
-            size="small"
+            sx={{ height: "50px" }}
             onChange={(e) => setPark(e.target.value)}
           >
-            <MenuItem value={"DL"}>Disneyland</MenuItem>
-            <MenuItem value={"DCA"}>Disney California Adventure</MenuItem>
-            <MenuItem value={"Both"}>Both</MenuItem>
+            <MenuItem value={"DL"}>
+              <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="flex-start"
+                spacing={2}
+              >
+                <Box>{getIcon("DL")}</Box> <Typography>Disneyland</Typography>
+              </Stack>
+            </MenuItem>
+            <MenuItem value={"DCA"}>
+              <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="flex-start"
+                spacing={2}
+              >
+                <Box>{getIcon("DCA")}</Box>{" "}
+                <Typography>Disney California Adventure</Typography>
+              </Stack>
+            </MenuItem>
+            <MenuItem value={"Both"}>
+              <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="flex-start"
+                spacing={2}
+              >
+                <Box>{getIcon("Both")}</Box> <Typography>Any Park</Typography>
+              </Stack>
+            </MenuItem>
           </Select>
         </FormControl>
 
-        <FormControl sx={{ m: 1, minWidth: 250 }}>
+        <FormControl sx={{ m: 1, minWidth: 150 }}>
           <InputLabel id="notificationUser-label">User to Notify</InputLabel>
           <Select
             labelId="notificationUser-label"
@@ -169,6 +230,7 @@ const Alert = () => {
             value={notificationUser}
             label="User to Notify"
             size="small"
+            sx={{ height: "50px" }}
             onChange={(e) => setNotificationUser(e.target.value)}
           >
             <MenuItem value={"bri"}>Bri</MenuItem>
@@ -179,13 +241,17 @@ const Alert = () => {
 
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
+            label="Choose Date"
             slotProps={{ textField: { size: "small" } }}
             sx={{
               "& .MuiInputBase-input": {
-                height: "23px",
+                height: "33px",
+              },
+
+              "& .MuiFormLabel-root": {
+                top: 4,
               },
             }}
-            label="Choose Date"
             value={date}
             onChange={(newValue) => setDate(newValue)}
           />
@@ -238,7 +304,11 @@ const Alert = () => {
                   <TableCell align="left">
                     {isSmall ? moment(alert.date).format("M-D") : alert.date}
                   </TableCell>
-                  <TableCell align="center">{alert.user}</TableCell>
+                  <TableCell align="center">
+                    {isSmall
+                      ? alert.user.substring(0, 1).toUpperCase()
+                      : alert.user}
+                  </TableCell>
                   {!isSmall && (
                     <TableCell align="left">
                       <Typography variant="caption">
@@ -246,14 +316,19 @@ const Alert = () => {
                       </Typography>
                     </TableCell>
                   )}
-                  <TableCell align="left">
+                  <TableCell
+                    align="left"
+                    sx={{ paddingLeft: "4px", paddingRight: "0" }}
+                  >
                     <Typography variant="caption">
                       {alert.lastFoundAt &&
-                        (isSmall
-                          ? moment(alert.lastFoundAt).format("h:mm")
-                          : moment(alert.lastFoundAt).format(
-                              "MMM D, h:mm:ss A"
-                            ))}
+                        (isSmall ? (
+                          <Typography sx={{ fontSize: "0.7rem" }}>
+                            {moment(alert.lastFoundAt).format("h:mm A")}
+                          </Typography>
+                        ) : (
+                          moment(alert.lastFoundAt).format("MMM D, h:mm:ss A")
+                        ))}
                     </Typography>
                   </TableCell>
                   {!isSmall && (
@@ -263,21 +338,37 @@ const Alert = () => {
                       </Typography>
                     </TableCell>
                   )}
-                  <TableCell align="right">
-                    <Stack direction="row">
+                  <TableCell
+                    align="right"
+                    sx={{ paddingLeft: "0px", paddingRight: "2px" }}
+                  >
+                    <Stack
+                      direction="row"
+                      justifyContent="flex-end"
+                      spacing={0}
+                    >
                       {!!alert.deactivatedAt && (
-                        <IconButton aria-label="reactivate">
-                          <EmojiEvents
+                        <IconButton sx={{ padding: 0 }} aria-label="reactivate">
+                          <PlayCircle
                             fontSize="small"
                             color="success"
                             onClick={() => reactivateAlert(alert)}
                           />
                         </IconButton>
                       )}
-                      <IconButton aria-label="delete">
+                      <IconButton sx={{ padding: 0 }} aria-label="delete">
                         <Delete
                           fontSize="small"
-                          onClick={() => deleteAlert(alert.id)}
+                          onClick={() => setAlertToDelete(alert)}
+                        />
+                      </IconButton>
+                      <IconButton sx={{ padding: 0 }} aria-label="more">
+                        <MenuBookTwoTone
+                          fontSize="small"
+                          sx={{
+                            color: blue[500],
+                          }}
+                          onClick={() => setAlertForDetails(alert)}
                         />
                       </IconButton>
                     </Stack>
@@ -289,6 +380,101 @@ const Alert = () => {
         </TableContainer>
       </Box>
       <Button onClick={logout}>Logout</Button>
+      <Dialog onClose={handleCloseDeleteModal} open={openDeleteModal}>
+        <DialogTitle>Are you sure you want to delete this alert?</DialogTitle>
+        <DialogContent>
+          <Stack
+            direction="row"
+            spacing={2}
+            alignItems="center"
+            justifyContent="flex-start"
+          >
+            <Box>{getIcon(alertToDelete?.park)}</Box>
+            <Typography>
+              {moment(alertToDelete?.date).format("dddd")}
+            </Typography>
+            <Typography>{alertToDelete?.date}</Typography>
+            <Typography>{alertToDelete?.user}</Typography>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="outlined" onClick={handleCloseDeleteModal}>
+            No, oops
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => deleteAlert(alertToDelete?.id)}
+          >
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog onClose={handleCloseDetailsModal} open={openDetailsModal}>
+        <DialogTitle>Alert Details</DialogTitle>
+        <DialogContent>
+          <Stack
+            spacing={2}
+            alignItems="flex-start"
+            justifyContent="flex-start"
+          >
+            <AlertDetailsRow>
+              <Typography>Park:</Typography>
+              <Stack direction="row" alignItems="center" spacing={3}>
+                {getIcon(alertForDetails?.park)}
+                <Typography>{alertForDetails?.park}</Typography>
+              </Stack>
+            </AlertDetailsRow>
+            <AlertDetailsRow>
+              <Typography>Date:</Typography>
+              <Typography>
+                {moment(alertForDetails?.date).format("dddd")},{" "}
+                {alertForDetails?.date}
+              </Typography>
+            </AlertDetailsRow>
+            <AlertDetailsRow>
+              <Typography>User:</Typography>
+              <Typography>{alertForDetails?.user}</Typography>
+            </AlertDetailsRow>
+            <AlertDetailsRow>
+              <Typography>Last Seen:</Typography>
+              <Typography>
+                {alertForDetails?.lastFoundAt &&
+                  moment(alertForDetails?.lastFoundAt).format(
+                    "MMM D, h:mm:ss A"
+                  )}
+              </Typography>
+            </AlertDetailsRow>
+            <AlertDetailsRow>
+              <Typography>Notification Count:</Typography>
+              <Typography>{alertForDetails?.notificationCount}</Typography>
+            </AlertDetailsRow>
+            <AlertDetailsRow>
+              <Typography>Status:</Typography>
+              {alertForDetails?.deactivatedAt ? (
+                <Chip label="Paused" color="warning" />
+              ) : (
+                <Chip label="Active" color="success" />
+              )}
+            </AlertDetailsRow>
+            <AlertDetailsRow>
+              <Typography>Created:</Typography>
+              <Typography>
+                {moment(alertForDetails?.createdAt).format("MMM D, h:mm:ss A")}
+              </Typography>
+            </AlertDetailsRow>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            onClick={handleCloseDetailsModal}
+            color={grey[700]}
+          >
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
@@ -345,6 +531,14 @@ const BothParksIcon = () => {
         
       </Box>
     </Icon>
+  );
+};
+
+const AlertDetailsRow = (props) => {
+  return (
+    <Stack direction="row" spacing={4} justifyContent="flex-start">
+      {props.children}
+    </Stack>
   );
 };
 
